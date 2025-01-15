5de8c77a35d19b3e3a4c955fe3c06c7bebac55f0
Alberto Boga <alberto.boggar@educa.jcyl.es>
2025-01-15 14:30:38 +0100

    Actualizo el nav

--- js/main.js
+++ js/main.js
@@ -5,12 +5,17 @@
 
     
     window.addEventListener('scroll', function() {
-        const nav = document.querySelector('ul');
-        console.log("Escrolleando");
+        const nav = document.querySelector('nav');
+        //const elements = this.document.getElementById("content-nav")
+        //console.log("Escrolleando");
         
-        if (window.scrollY > 50) { 
-          nav.classList.add('nav-scrolled');
+        if (window.scrollY > 20) {
+          nav.classList.remove("navegacion");
+         nav.classList.add('nav-scrolled');
+        //elements.classList.add('nav-scrolled');  
         } else {
           nav.classList.remove('nav-scrolled');
+          nav.classList.add("navegacion");
+          //elements.classList.remove('nav-scrolled');
         }
       });
